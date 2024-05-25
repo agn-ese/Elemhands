3c2895de533c8c636c7af4c42e77ee7915095a1a
Agnese <s305900@studenti.polito.it>
2024-05-25 13:52:17 +0200

    Braccia funzionanti
    To-do: movimento mani

--- Assets/VR Body/IKTargetFollowVRRig.cs
+++ Assets/VR Body/IKTargetFollowVRRig.cs
@@ -30,7 +30,7 @@ public class IKTargetFollowVRRig : MonoBehaviour
     {
         transform.position = head.ikTarget.position + headBodyPositionOffset;
         float yaw = head.vrTarget.eulerAngles.y;
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),turnSmoothness);
+       transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),turnSmoothness);
 
         head.Map();
         leftHand.Map();
