08073a9a715fcd854746c4c6e7e3fb396160ac64
Andrea DAngelo <dangeloandrea1999@gmail.com>
2024-06-06 20:16:57 +0200

    Inserimento bozza primo enigma per IEEE (sistemare interazione con chiave)

---
+++ Assets/InsertKey.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class InsertKey : MonoBehaviour
+{
+    [SerializeField] private SplineAnimate spline;
+    [SerializeField] private AudioSource sound;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Key"))
+        {
+            spline.Play();
+            sound.Play();
+        }
+    }
+}
--- Assets/Solleva.cs
+++ Assets/Solleva.cs
@@ -1,4 +1,6 @@
 using Oculus.Interaction;
+using Oculus.Interaction.DistanceReticles;
+using Oculus.Interaction.Locomotion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +13,16 @@ public class Solleva : MonoBehaviour
     private IActiveState state { get; set; }
 
     public Transform father;
-    Vector3 newPosition = new(0, 0, 3);
+    Vector3 newPosition = new(0, 0, 6);
     [SerializeField] private bool isInAir = false;
+    [SerializeField] private bool waitTime = false;
+    [SerializeField] private float time = 3f;
     Rigidbody rigidbody;
 
     [SerializeField] private TutorialManager manager;
+    [SerializeField] private Transform target;
+    [SerializeField] private TeleportInteractable[] teleports;
+    [SerializeField] private ReticleDataTeleport[] reticles;
 
     private void Awake()
     {
@@ -24,24 +31,57 @@ public class Solleva : MonoBehaviour
         this.AssertField(state, nameof(state));
     }
 
+    private void Update()
+    {
+        if(waitTime)
+        {
+            time -= Time.deltaTime;
+            if (time < 0)
+            {
+                waitTime = false;
+                time = 5f;
+            }
+        }
+    }
+
     public void SollevaOggetto()
     {
         if(state.Active)
         {
-            if(!isInAir)
+            if(!isInAir && !waitTime)
             {
+                waitTime = true;
                 transform.SetParent(father);
                 transform.localPosition = newPosition;
                 isInAir = true;
                 rigidbody.useGravity = false;
                 rigidbody.isKinematic = true;
                 manager.DialogoCassaSollevata();
-            } else
+                
+            } else if(isInAir && !waitTime)
             {
+                waitTime = true;
                 transform.SetParent(null);
                 isInAir = false;
-                rigidbody.useGravity = true;
-                rigidbody.isKinematic = false;
+                if (Vector3.Distance(target.position, transform.position) <= 5f)
+                {
+                    transform.position = target.position;
+                    rigidbody.useGravity = false;
+                    rigidbody.isKinematic = true;
+                    foreach (TeleportInteractable teleport in teleports)
+                    {
+                        teleport.AllowTeleport = true;
+                    }
+                    foreach (ReticleDataTeleport reticle in reticles)
+                    {
+                        reticle.ReticleMode = ReticleDataTeleport.TeleportReticleMode.ValidTarget;
+                    }
+                } else
+                {
+                    rigidbody.useGravity = true;
+                    rigidbody.isKinematic = false;
+                }
+                manager.DialogoTrasportoCassa();
             }
         }
     }
