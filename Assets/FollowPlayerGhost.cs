dd1bbcfd07086918d67f83a8c2960f2aa1bb1830
Agnese <s305900@studenti.polito.it>
2024-10-03 11:45:22 +0200

    Risolto problema mano animata

--- Assets/FollowPlayerGhost.cs
+++ Assets/FollowPlayerGhost.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class FollowPlayerGhost : MonoBehaviour
 {
     [SerializeField] private Transform objectToFollow;
-    [SerializeField] private Vector3 offset;
+    //[SerializeField] private Vector3 offset;
 
-    [SerializeField] private float yPos = 0.0f;
+    //[SerializeField] private float yPos = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(objectToFollow.transform.position.x, yPos, objectToFollow.transform.position.z) + offset;
+        //transform.position = new Vector3(objectToFollow.transform.position.x, yPos, objectToFollow.transform.position.z) + offset;
         transform.rotation = objectToFollow.rotation;
+        //transform.position = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width* offset.x, Screen.height*offset.y, offset.z));
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * (- 1/4), Screen.height * (-2.5f), 0.5f));
+        Debug.Log(Screen.width + " " + Screen.height);
 
     }
 
