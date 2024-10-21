7e60593b08c4fa4cf3f57e495c353c4723795e32
michelebissanti <michele.bissanti01@gmail.com>
2024-10-21 16:06:18 +0200

    sistemati prefab spawn

--- Assets/PotereTerra.cs
+++ Assets/PotereTerra.cs
@@ -11,21 +11,21 @@ public class PotereTerra : MonoBehaviour
     [Tooltip("The IActiveState to debug.")]
     [SerializeField, Interface(typeof(IActiveState))]
     private UnityEngine.Object _activeState;
-    private IActiveState state { get; set; }
+    //private IActiveState state { get; set; }
 
     // prendo il prefab dell'oggetto da evocare che si chiama "OggettoEvocato"
     [SerializeField] private GameObject oggettoEvocato;
 
-    private void Awake()
-    {
-        state = _activeState as IActiveState;
-        this.AssertField(state, nameof(state));
-    }
+    /*     private void Awake()
+        {
+            state = _activeState as IActiveState;
+            this.AssertField(state, nameof(state));
+        } */
 
     public void EvocaOggetto()
     {
         // se premo il tasto F o se premo il tasto B del controller meta quest
-        if (state.Active || Input.GetKeyDown(KeyCode.F) || OVRInput.GetDown(OVRInput.Button.Two))
+        if (/* state.Active || */ Input.GetKeyDown(KeyCode.F) || OVRInput.GetDown(OVRInput.Button.Two))
         {
             // creo un nuovo oggetto che posiziono davanti al giocatore
             GameObject oggetto = Instantiate(oggettoEvocato, transform.position + transform.forward * 2, Quaternion.identity);
